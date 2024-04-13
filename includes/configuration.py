3069827e91c70afa72b8ce0a6f8e055a8725d2f3
AkshatPote <45456502+AkshatPote@users.noreply.github.com>
2024-04-13 19:46:13 +0530

    Add files via upload

---
+++ includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dlstoragedemo/raw"
+processed_folder_path = "/mnt/formula1dlstoragedemo/processed"
+presentation_folder_path = "/mnt/formula1dlstoragedemo/presentation"
+
+# COMMAND ----------
+
+#raw_folder_path = 'abfss://raw@formula1dlstoragedemo.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@formula1dlstoragedemo.dfs.core.windows.net'
+#presentation_folder_path = 'abfss://presentation@formula1dlstoragedemo.dfs.core.windows.net'
